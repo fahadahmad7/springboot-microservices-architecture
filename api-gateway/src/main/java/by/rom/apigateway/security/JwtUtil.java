16c966b95e17caa620003655c7d892eac8c93d0a
fahad <fahadahmadkmj@gmail.com>
2026-06-17 05:15:39 +0530

    Added dependency

--- api-gateway/src/main/java/by/rom/apigateway/security/JwtUtil.java
+++ api-gateway/src/main/java/by/rom/apigateway/security/JwtUtil.java
@@ -27,7 +27,7 @@ public class JwtUtil {
                 .setSubject(username)
                 .claim("role", "USER")
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // 1 hour
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60000)) // 1 hour
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
